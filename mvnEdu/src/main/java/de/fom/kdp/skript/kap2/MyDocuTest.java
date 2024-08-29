03b2a71ad87fcd722a6582d9258200659a51ce16
Pro Engels <PE@softwareengel.de>
2024-08-29 15:14:27 +0200

    ML

---
+++ mvnEdu/src/main/java/de/fom/kdp/skript/kap2/MyDocuTest.java
@@ -0,0 +1,21 @@
+package de.fom.kdp.skript.kap2;
+
+/**
+ * MyDocuTest Dokumentation
+ * 
+ * @author engels
+ *
+ */
+public class MyDocuTest {
+	/** das jahr … */
+	public int jahr;
+
+	/**
+	 * print Method for ...
+	 * 
+	 * @param s string aof printing out
+	 */
+	public void print(String s) {
+		System.out.println("Ausgabe: " + s);
+	}
+}
